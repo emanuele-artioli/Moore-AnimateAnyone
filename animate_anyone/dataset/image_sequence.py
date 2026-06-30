140ef1a83aa3e3aef4005feee59697fd6638ed1a
emanuele-artioli <emanuele.artioli@aau.at>
2026-06-30 09:31:26 +0200

    Fix stage 2 dataset keys

--- animate_anyone/dataset/image_sequence.py
+++ animate_anyone/dataset/image_sequence.py
@@ -182,7 +182,7 @@ class ImageSequenceDataset(Dataset):
         return {
             "dataset_name": "image_sequence",
             # Keys for stage 2
-            "pixel_values": tgt_img_tensor,
+            "pixel_values_vid": tgt_img_tensor,
             "pixel_values_pose": tgt_pose_tensor,
             "clip_ref_image": clip_ref_img_tensor,
             "pixel_values_ref_img": ref_img_tensor,
